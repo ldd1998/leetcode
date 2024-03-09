735fd0dccf0fc7d0e9849b7cca9967caf5d4f984
‘ldd1998’ <1193804498@qq.com>
2024-03-09 18:56:14 +0800

    爬楼梯

--- 70. 爬楼梯/src/Main.java
+++ 70. 爬楼梯/src/Main.java
@@ -1,16 +1,13 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println(climbStairs(6));
     }
     public static int climbStairs(int n) {
         int memo[] = new int[n + 1];
-        return climb_Stairs(n, memo);
+        return climbStairs(n, memo);
     }
 
-    public static int climb_Stairs(int n, int memo[]) {
+    public static int climbStairs(int n, int memo[]) {
         if (n == 1) {
             return 1;
         }
@@ -19,7 +16,7 @@ public class Main {
         }
         // 如果memo[n]为0，说明还没计算过这个值
         if (memo[n] == 0) {
-            memo[n] = climb_Stairs(n - 1, memo) + climb_Stairs(n - 2, memo);
+            memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
         }
         // 这个值在备忘录中，返回之
         return memo[n];
